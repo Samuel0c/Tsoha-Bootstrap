414c20b27f8395a3402533cc7a0fef74d90aa2ba
Samuel0c <sleppiniemi@protonmail.com>
2017-12-26 17:16:50 +0200

    update documentation and new create_tables_sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,29 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE RegisteredUser(
+  id SERIAL PRIMARY KEY,
+  username varchar(25) NOT NULL,
+  password varchar(50) NOT NULL
+);
+
+CREATE TABLE Priority(
+  importance_value INTEGER PRIMARY KEY CHECK (importance_value > 0 AND importance_value < 6),
+);
+
+CREATE TABLE Topic(
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL,
+);
+
+CREATE TABLE Task(
+  id SERIAL PRIMARY KEY,
+  task_name varchar(60) NOT NULL,
+  status boolean DEFAULT FALSE,
+  notes varchar(300),
+  owner_id INTEGER REFERENCES RegisteredUser(id),
+  priority_id INTEGER REFERENCES Priority(id),
+);
+
+CREATE TABLE Task_topic(
+  task_id INTEGER REFERENCES Task(id),
+  topic_id INTEGER REFERENCES Topic(id)
+);
+
