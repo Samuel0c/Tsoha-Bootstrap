37c67fb216e9a039b1b8fd2b97da21b698b74d95
Samuel0c <sleppiniemi@protonmail.com>
2017-12-28 18:07:23 +0200

    more test data

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -4,8 +4,16 @@ INSERT INTO RegisteredUser (username, password) VALUES ('Adenine', 'kukkaruukku'
 
 INSERT INTO Priority (importance_value) VALUES ('3');
 
+INSERT INTO Priority (importance_value) VALUES ('4');
+
 INSERT INTO Topic (name) VALUES ('Gardening');
+
+INSERT INTO Topic (name) VALUES ('Photography');
 
 INSERT INTO Task (task_name, status, notes, owner_id, priority) VALUES ('Plant the beans outside', 'false','Remember to place the beans next to the potatos and leave enough space between the plants.', '1', '3');
 
-INSERT INTO Task_topic (task_id, topic_id) VALUES ('1', '1')
+INSERT INTO Task (task_name, status, notes, owner_id, priority) VALUES ('Edit new photos', 'false','Edit the pictures from Suvilahti photo shoot.', '2', '4');
+
+INSERT INTO Task_topic (task_id, topic_id) VALUES ('1', '1');
+
+INSERT INTO Task_topic (task_id, topic_id) VALUES ('2', '2');
