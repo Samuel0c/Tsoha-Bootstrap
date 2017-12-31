aff19e81024a618c57a302aeaaabc45fb51d200c
Samuel0c <sleppiniemi@protonmail.com>
2017-12-31 16:22:24 +0200

    removed outdated code from HelloWorldController

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -27,28 +27,8 @@ class HelloWorldController extends BaseController {
         Kint::dump($priority);
     }
 
-    public static function todo_list() {
-        View::make('todo_list.html');
-    }
-
-    public static function edit_task() {
-        View::make('edit_task.html');
-    }
-
-    public static function show_task() {
-        View::make('show_task.html');
-    }
-
-    public static function topic_list() {
-        View::make('topic_list.html');
-    }
-
     public static function add_task() {
         View::make('add_task.html');
-    }
-
-    public static function edit_topic() {
-        View::make('edit_topic.html');
     }
 
     public static function login() {
